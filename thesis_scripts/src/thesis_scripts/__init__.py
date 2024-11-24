4edecd2c119d4afc40c07a1ff1107cfcab38f4f6
jacopok <jacopok@gmail.com>
2024-11-24 19:20:52 +0100

    new writing about NS

--- thesis_scripts/src/thesis_scripts/__init__.py
+++ thesis_scripts/src/thesis_scripts/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from thesis-scripts!"
+import matplotlib.pyplot as plt
+from pathlib import Path
+plt.rc('text', usetex=True)
+
+data_path = Path('.').resolve().parent.parent / 'data'
+
